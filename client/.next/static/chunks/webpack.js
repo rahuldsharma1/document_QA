afefade9747e5d8bb045a67f4bfbc286b451a9cb
Praneet Nadella <40508942+praneetnadella@users.noreply.github.com>
2025-02-20 17:09:21 -0800

    got docker to work and add.gitignore

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44c076a747036bc0"; }
+/******/ 		__webpack_require__.h = function() { return "70ae6815a7bf6bfa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
